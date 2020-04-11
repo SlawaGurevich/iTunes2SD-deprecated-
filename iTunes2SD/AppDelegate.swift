f5c5bf74d9beed39aad6b833e7da6df9418819f3
slawa <slawa@slawagurevich.com>
2020-04-11 19:48:07 +0200

    Initial Commit

---
+++ iTunes2SD/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iTunes2SD
+//
+//  Created by Slawa on 11.04.20.
+//  Copyright © 2020 slawa.gurevich. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iTunes2SD/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iTunes2SD
+//
+//  Created by Slawa on 11.04.20.
+//  Copyright © 2020 slawa.gurevich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
